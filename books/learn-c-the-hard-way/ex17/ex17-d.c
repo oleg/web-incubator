6597a257254440ed32bcc4c4fd8767ef1a3f0365
Oleg Prozorov <pro.oleg@gmail.com>
2019-09-13 20:43:02 +0200

    Implement dynamic name and email

--- books/learn-c-the-hard-way/ex17/ex17-d.c
+++ books/learn-c-the-hard-way/ex17/ex17-d.c
@@ -1,20 +1,20 @@
+
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
 
-#define MAX_DATA 512
-//#define MAX_ROWS 100
 
 struct Address {
   int id;
   int set;
-  char name[MAX_DATA];
-  char email[MAX_DATA];
+  char *name;
+  char *email;
 };
 
 struct Database {
+  size_t max_data;
   size_t max_rows;
   struct Address *rows;
 };
@@ -40,6 +40,12 @@ void Address_print(struct Address *addr)
   printf("%d %s %s\n", addr->id, addr->name, addr->email);
 }
 
+size_t Database_dataSize(size_t max_data)
+{
+  return sizeof(char) * max_data;
+}
+
+
 size_t Database_rowsSize(size_t max_rows)
 {
   return sizeof(struct Address) * max_rows;
@@ -60,44 +66,107 @@ struct Connection *Connection_create()
 
 void Connection_openFile(struct Connection *conn, const char *filename, const char *mode)
 {
-  conn->file = fopen(filename, mode); //w or r+
+  conn->file = fopen(filename, mode);
   if (!conn->file)
     die("Failed to open the file.");
 }
 
-struct Connection *Database_create(const char *filename, int max_rows)
+void Address_create(struct Database *db)
+{
+  db->rows = malloc(Database_rowsSize(db->max_rows));
+  int i = 0;
+  for (; i < db->max_rows; i++) {
+    
+    struct Address addr = {.id=i,.set=0};
+    addr.name = malloc(Database_dataSize(db->max_data));
+    addr.email = malloc(Database_dataSize(db->max_data));
+    
+    db->rows[i] = addr;
+  }
+}
+
+struct Connection *Database_create(const char *filename, int max_rows, int max_data)
 {
   struct Connection *conn = Connection_create();
   Connection_openFile(conn, filename, "w");
   conn->db = malloc(Database_size());
   conn->db->max_rows = max_rows;
-  conn->db->rows = malloc(Database_rowsSize(conn->db->max_rows));
-  
-  int i = 0;
-  for (i = 0; i < conn->db->max_rows; i++) {
-    struct Address addr = { .id = i, .set = 0 };
-    conn->db->rows[i] = addr;
-  }
+  conn->db->max_data = max_data;
+
+  Address_create(conn->db);
   return conn;
 }
 
 struct Connection *Database_open(const char *filename)
 {
+  int rc;
+  
   struct Connection *conn = Connection_create();
   Connection_openFile(conn, filename, "r+");
+  
   conn->db = malloc(Database_size());
-    
-  int rc;
   rc = fread(conn->db, Database_size(), 1, conn->file);
   if (rc != 1)
     die("Failed to load database.");
 
-  conn->db->rows = malloc(Database_rowsSize(conn->db->max_rows));
-  rc = fread(conn->db->rows, Database_rowsSize(conn->db->max_rows), 1, conn->file);
+  size_t data_size = Database_dataSize(conn->db->max_data);
+  conn->db->rows = malloc(Database_rowsSize(conn->db->max_rows)); 
+  int i = 0;
+  for (i = 0; i < conn->db->max_rows; i++) {
+    struct Address *addr = &conn->db->rows[i];
+    rc = fread(addr, sizeof(struct Address), 1, conn->file);
+    if (rc != 1)
+      die("Failed to load rows.");
+    
+    addr->name = malloc(data_size);
+    rc = fread(addr->name, data_size, 1, conn->file);
+    if (rc != 1)
+      die("Failed to load rows.");
+
+    addr->email = malloc(data_size);
+    rc = fread(addr->email, data_size, 1, conn->file);
+    if (rc != 1)
+      die("Failed to load rows.");
+  }
+
   if (rc != 1)
     die("Failed to load rows.");
+  
   return conn;
+}
 
+void Database_write(struct Connection *conn)
+{
+  rewind(conn->file);
+
+  int rc;
+  
+  rc = fwrite(conn->db, Database_size(), 1, conn->file);
+  if (rc != 1)
+    die("Failed to write database.");
+
+  size_t data_size = Database_dataSize(conn->db->max_data);
+  int i = 0;
+  for (i = 0; i < conn->db->max_rows; i++) {
+    
+    struct Address addr =  conn->db->rows[i];
+
+    rc = fwrite(&addr, sizeof(struct Address), 1, conn->file);
+    if (rc != 1)
+      die("Failed to write database.");
+    
+    rc = fwrite(addr.name, data_size, 1, conn->file);
+    if (rc != 1)
+      die("Failed to write database.");
+
+    rc = fwrite(addr.email, data_size, 1, conn->file);
+    if (rc != 1)
+      die("Failed to write database.");
+  }
+
+  rc = fflush(conn->file);
+  if (rc == -1)
+    die("Cannot flush database.");
 }
 
 void Database_close(struct Connection *conn)
@@ -116,23 +185,6 @@ void Database_close(struct Connection *conn)
   }
 }
 
-void Database_write(struct Connection *conn)
-{
-  rewind(conn->file);
-
-  int rc = fwrite(conn->db, Database_size(), 1, conn->file);
-  if (rc != 1)
-    die("Failed to write database.");
-  
-  rc = fwrite(conn->db->rows, Database_rowsSize(conn->db->max_rows), 1, conn->file);
-  if (rc != 1)
-    die("Failed to write addresses.");
-
-  rc = fflush(conn->file);
-  if (rc == -1)
-    die("Cannot flush database.");
-}
-
 void Database_set(struct Connection *conn, int id, const char *name, const char *email)
 {
   struct Address *addr = &conn->db->rows[id];
@@ -140,13 +192,17 @@ void Database_set(struct Connection *conn, int id, const char *name, const char 
     die("Already set, delete it first");
 
   addr->set = 1;
-  char *res = strncpy(addr->name, name, MAX_DATA);
+  char *res;
+  res = strncpy(addr->name, name, conn->db->max_data);
+
   if (!res)
     die("Name copy failed");
 
-  res = strncpy(addr->email, email, MAX_DATA);
+  res = strncpy(addr->email, email, conn->db->max_data);
   if (!res)
     die("Email copy failed");
+
+
 }
 
 void Database_get(struct Connection *conn, int id)
@@ -161,7 +217,13 @@ void Database_get(struct Connection *conn, int id)
 
 void Database_delete(struct Connection *conn, int id)
 {
-  struct Address addr = { .id = id, .set = 0};
+  struct Address addr =
+    {
+     .id = id,
+     .set = 0,
+     .name = malloc(Database_dataSize(conn->db->max_data)),
+     .email = malloc(Database_dataSize(conn->db->max_data))
+    };
   conn->db->rows[id] = addr;
 }
 
@@ -189,8 +251,8 @@ int main(int argc, char *argv[])
   int id;
 
   switch(action) {
-  case 'c': 
-    conn = Database_create(filename, atoi(argv[3]));
+  case 'c':
+    conn = Database_create(filename, atoi(argv[3]), atoi(argv[4]));
     Database_write(conn);
     break;
   
--- books/learn-c-the-hard-way/ex17/test-ex17-d.sh
+++ books/learn-c-the-hard-way/ex17/test-ex17-d.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -ex
 touch db
 rm db
-./ex17-d db c 90
+./ex17-d db c 9 512
 ./ex17-d db l
 ./ex17-d db s 1 "Oleg Pro" "oleg@example.com"
 ./ex17-d db s 5 "John Doe" "john.doe@example.com"
