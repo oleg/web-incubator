f171f2d2b8b0c40a9f9eadd8a549bb0fd89594a8
Oleg Prozorov <pro.oleg@gmail.com>
2019-09-25 21:20:36 +0200

    Learn C; Fix cproject template

--- books/learn-c-the-hard-way/cproject/test/runtests.sh
+++ books/learn-c-the-hard-way/cproject/test/runtests.sh
@@ -1,16 +1,16 @@
 echo "Running unit tests:"
 
-for i in tests/*_tests
+for i in test/*_test
 do
     if test -f $i
     then
-        if $VALGRIND ./$i 2>> tests/tests.log
+        if $VALGRIND ./$i 2>> test/tests.log
         then
             echo $i PASS
         else
-            echo "ERROR in test $i: here's tests/tests.log"
+            echo "ERROR in test $i: here's test/tests.log"
             echo "------"
-            tail tests/tests.log
+            tail test/tests.log
             exit 1
         fi
     fi
