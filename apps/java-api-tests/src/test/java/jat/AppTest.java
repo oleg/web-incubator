2b8fb9936f1f93f902216153765e8e8545a7a42f
Oleg Prozorov <pro.oleg@gmail.com>
2019-10-07 21:17:43 +0200

    Add Java api tests project

---
+++ apps/java-api-tests/src/test/java/jat/AppTest.java
@@ -0,0 +1,13 @@
+package jat;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AppTest {
+
+    @Test
+    void testApp() {
+        assertTrue(true);
+    }
+}
