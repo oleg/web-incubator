a91a59c35081ed5664e930cb3717a6a054729653
Oleg Prozorov <pro.oleg@gmail.com>
2020-10-27 21:16:44 +0100

    Go Maze; more tests for init Grid

--- books/mazes-in-go/grid_test.go
+++ books/mazes-in-go/grid_test.go
@@ -28,27 +28,44 @@ func Test_sets_correct_rows_and_columns(t *testing.T) {
 }
 
 func Test_sets_north_south_east_west_sides(t *testing.T) {
+	grid := NewGrid(3, 3)
+
+	a := grid.cells[0][0]
+	b := grid.cells[0][1]
+	c := grid.cells[0][2]
+
+	d := grid.cells[1][0]
+	e := grid.cells[1][1]
+	f := grid.cells[1][2]
+
+	g := grid.cells[2][0]
+	h := grid.cells[2][1]
+	i := grid.cells[2][2]
+
+	n := (*Cell)(nil)
+
 	//	a|b|c
 	//	d|e|f
 	//	g|h|i
 
-	grid := NewGrid(3, 3)
-	a := grid.cells[0][0]
-	b := grid.cells[0][1]
-	//c := grid.cells[0][2]
-	//
-	d := grid.cells[1][0]
-	//e := grid.cells[1][1]
-	//f := grid.cells[1][2]
-	//
-	//g := grid.cells[2][0]
-	//h := grid.cells[2][1]
-	//i := grid.cells[2][2]
+	assertNorthEastSouthWest(t, a, n, b, d, n)
+	assertNorthEastSouthWest(t, b, n, c, e, a)
+	assertNorthEastSouthWest(t, c, n, n, f, b)
 
-	assert.Nil(t, a.north)
-	assert.Equal(t, b, a.east)
-	assert.Equal(t, d, a.south)
-	assert.Nil(t, a.west)
+	assertNorthEastSouthWest(t, d, a, e, g, n)
+	assertNorthEastSouthWest(t, e, b, f, h, d)
+	assertNorthEastSouthWest(t, f, c, n, i, e)
+
+	assertNorthEastSouthWest(t, g, d, h, n, n)
+	assertNorthEastSouthWest(t, h, e, i, n, g)
+	assertNorthEastSouthWest(t, i, f, n, n, h)
+}
+
+func assertNorthEastSouthWest(t *testing.T, c, north, east, south, west *Cell) {
+	assert.Equal(t, north, c.north)
+	assert.Equal(t, east, c.east)
+	assert.Equal(t, south, c.south)
+	assert.Equal(t, west, c.west)
 }
 
 func Test_creates_new_grid_with_100_cells(t *testing.T) {
@@ -92,14 +109,22 @@ func Test_access_returns_nil_for_incorrect_coordinates(t *testing.T) {
 	assert.Nil(t, grid.Cell(0, 8))
 }
 
-//func Test_iterate_over_all_cells_correct_cells(t *testing.T) {
-//	grid := NewGrid(3, 3)
-//	grid.cells[0][0] = NewCell()
-//
-//	count := 0
-//	grid.EachCells(func(cell *Cell) {
-//		count++
-//	})
-//
-//	assert.Equal(t, 49, count)
-//}
+func Test_iterate_over_all_cells_correct_cells(t *testing.T) {
+	grid := NewGrid(3, 2)
+	visitedCells := make([]*Cell, 0)
+
+	grid.EachCells(func(cell *Cell) {
+		visitedCells = append(visitedCells, cell)
+	})
+
+	assert.Len(t, visitedCells, 6)
+
+	assert.Equal(t, grid.cells[0][0], visitedCells[0])
+	assert.Equal(t, grid.cells[0][1], visitedCells[1])
+
+	assert.Equal(t, grid.cells[1][0], visitedCells[2])
+	assert.Equal(t, grid.cells[1][1], visitedCells[3])
+
+	assert.Equal(t, grid.cells[2][0], visitedCells[4])
+	assert.Equal(t, grid.cells[2][1], visitedCells[5])
+}
