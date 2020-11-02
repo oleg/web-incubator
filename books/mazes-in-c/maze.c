e9d329582a9fb1efd4f51fe1c967c0a2342b4a72
Oleg Prozorov <pro.oleg@gmail.com>
2020-11-02 21:45:58 +0100

    C Maze; create Row

--- books/mazes-in-c/maze.c
+++ books/mazes-in-c/maze.c
@@ -7,31 +7,67 @@
 typedef struct Row Row;
 struct Row {
   size_t n_cells;
-  Cell* cells[];
+  Cell cells[];
 };
 
 typedef struct Grid Grid;
 struct Grid {
   size_t n_rows;
   Row* rows[];
+  //  Cell** cells[]???
 };
 
- /* st_employees* make_employees(unsigned n) { */
- /*    st_employees* s = malloc(sizeof(s_employees)+n*sizeof(char*)); */
- /*    if (!s) { perror("malloc make_employees"); exit(EXIT_FAILURE); }; */
- /*    s->No_of_Employees = n; */
- /*    for (unsigned i=0; i<n; i++) s->Employe_Names[i] = NULL; */
- /*    return s; */
- /* } */
+Row* Row_create(size_t n) {
+  Row* row = malloc(sizeof(Row)+n*sizeof(Cell));
+  if (!row) {
+    printf("ERROR: failed to create Row\n");
+    return NULL;
+  }
+  row->n_cells = n;
+  for (size_t i = 0; i < n; i++) {
+    Cell c = row->cells[i];
+    c.row = 0;
+    c.column = 0;
+    Cell_set_north(&c, NULL);
+    Cell_set_east(&c, NULL);
+    Cell_set_south(&c, NULL);
+    Cell_set_west(&c, NULL);
+  }
+  return row;
+}
 
 Grid* Grid_create(size_t rows, size_t columns) {
-  Grid* grid = malloc(sizeof(Grid)+rows*(sizeof(Row)));
+  Grid* grid = malloc(sizeof(Grid)+rows*(sizeof(Row*)));
   if (!grid) {
     //todo return or print an error message?
     return NULL;
   }
   grid->n_rows = rows;
+  for (size_t i = 0; i < rows; i++) {
+    Row* row = Row_create(columns);
+    if (row == NULL) {
+      //todo cleanup, error message?
+      return NULL;
+    }
+    grid->rows[i] = row;
+  }
   return grid;
+}
+
+void test_create_rows_with_correct_sizes() {
+  printf("> should create row with correct number of cells\n");
+  
+  Row* r = Row_create(6);
+  
+  if (r == NULL)
+    printf("ERROR: failed to create row\n");
+  
+  if (r->n_cells != 6)
+    printf("ERROR: wrong number of cells\n");
+
+  for (size_t i = 0; i < r->n_cells; i++)
+    if (r->cells[i].column != 3)
+      printf("ERROR: Cell %zu is not correctly initialized\n", i);
 }
 
 void test_create_grid_non_null() {
@@ -63,6 +99,7 @@ void test_create_grid_correct_sizes() {
 
 
 void Grid_run_tests() {
+  test_create_rows_with_correct_sizes();
   test_create_grid_non_null();
   test_create_grid_correct_sizes();
 }
