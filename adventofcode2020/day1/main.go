0617c7f1dd836c1e983564c5f706b0a7553342bd
Oleg Prozorov <pro.oleg@gmail.com>
2020-12-05 17:23:50 +0100

    Advent of Code 2020; day1 task2

--- adventofcode2020/day1/main.go
+++ adventofcode2020/day1/main.go
@@ -7,8 +7,10 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	a, b := find1(ints)
-	println(a * b)
+	a1, b1 := find1(ints)
+	println(a1 * b1)
+	a2, b2, c2 := find2(ints)
+	println(a2 * b2 * c2)
 }
 
 func find1(input []int) (int, int) {
@@ -22,13 +24,15 @@ func find1(input []int) (int, int) {
 	return 0, 0
 }
 
-func find2(input []int) (int, int) {
+func find2(input []int) (int, int, int) {
 	for _, a := range input {
 		for _, b := range input {
-			if a != b && a+b == 2020 {
-				return a, b
+			for _, c := range input {
+				if a != b && a != c && b != c && a+b+c == 2020 {
+					return a, b, c
+				}
 			}
 		}
 	}
-	return 0, 0
+	return 0, 0, 0
 }
