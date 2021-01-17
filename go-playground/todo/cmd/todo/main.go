a551fe2389b3253de2136260a10e03442df0481c
Oleg Prozorov <pro.oleg@gmail.com>
2021-01-17 19:55:20 +0100

    Go cli; use flag pacakge

--- go-playground/todo/cmd/todo/main.go
+++ go-playground/todo/cmd/todo/main.go
@@ -1,30 +1,49 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/oleg/incubator/go-playground/todo"
 	"os"
-	"strings"
 )
 
 const todoFileName = ".todo.json"
 
 func main() {
+	task := flag.String("task", "", "Task to be added")
+	list := flag.Bool("list", false, "List all tasks")
+	complete := flag.Int("complete", 0, "Item to be completed")
+	flag.Parse()
+
 	l := &todo.List{}
 	if err := l.Get(todoFileName); err != nil {
 		_, _ = fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 	switch {
-	case len(os.Args) == 1:
+	case *list:
 		for _, item := range *l {
-			fmt.Println(item.Task)
+			if !item.Done {
+				fmt.Println(item.Task)
+			}
 		}
-	default:
-		l.Add(strings.Join(os.Args[1:], " "))
+	case *complete > 0:
+		if err := l.Complete(*complete); err != nil {
+			_, _ = fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
 		if err := l.Save(todoFileName); err != nil {
 			_, _ = fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
+	case *task != "":
+		l.Add(*task)
+		if err := l.Save(todoFileName); err != nil {
+			_, _ = fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	default:
+		_, _ = fmt.Fprintln(os.Stderr, "Invalid option")
+		os.Exit(1)
 	}
 }
--- go-playground/todo/cmd/todo/main_test.go
+++ go-playground/todo/cmd/todo/main_test.go
@@ -5,7 +5,6 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
-	"strings"
 	"testing"
 )
 
@@ -37,14 +36,14 @@ func TestTodoCLI(t *testing.T) {
 	cmdPath := filepath.Join(dir, binName)
 
 	t.Run("AddNewTask", func(t *testing.T) {
-		cmd := exec.Command(cmdPath, strings.Split(task, " ")...)
+		cmd := exec.Command(cmdPath, "-task", task)
 		if err := cmd.Run(); err != nil {
 			t.Fatal(err)
 		}
 	})
 
 	t.Run("ListTasks", func(t *testing.T) {
-		cmd := exec.Command(cmdPath)
+		cmd := exec.Command(cmdPath, "-list")
 		out, err := cmd.CombinedOutput()
 		if err != nil {
 			t.Fatal(err)
