91d555fdb046788c1e2eab05bfa9501d8cf2f0f5
Oleg Prozorov <oleg.entw@gmail.com>
2021-04-09 19:27:26 +0200

    rust: minigrep working version

--- the-rust-programming-language/minigrep/src/main.rs
+++ the-rust-programming-language/minigrep/src/main.rs
@@ -1,6 +1,6 @@
+use minigrep::Config;
 use std::env;
 use std::process;
-use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,12 +8,9 @@ fn main() {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
 
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
         process::exit(1);
     }
 }
-
