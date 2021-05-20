d9a3a51009cf865428bf284f902af7da9f91899f
Oleg Prozorov <oleg@fraugster.com>
2021-05-20 21:32:47 +0200

    go-dser: start grpc implementation

---
+++ go-dser/internal/server/server.go
@@ -0,0 +1,42 @@
+package server
+
+import (
+	"context"
+	api "github.com/oleg/incubator/go-dser/api/v1"
+)
+
+type Config struct {
+	CommitLog CommitLog
+}
+
+var _ api.LogServer = (*grpcServer)(nil)
+
+type grpcServer struct {
+	api.UnimplementedLogServer
+	*Config
+}
+
+func newgrpcServer(config *Config) (srv *grpcServer, err error) {
+	srv = &grpcServer{
+		Config: config,
+	}
+	return srv, nil
+}
+
+func (s *grpcServer) Produce(ctx context.Context, req *api.ProduceRequest) (*api.ProduceResponse, error) {
+	offset, err := s.CommitLog.Append(req.Record)
+	if err != nil {
+		return nil, err
+	}
+	return &api.ProduceResponse{Offset: offset}, nil
+}
+
+
+func (s *grpcServer) Consume() {
+
+}
+
+type CommitLog interface {
+	Append(*api.Record) (uint64, error)
+	Read(uint64) (*api.Record, error)
+}
