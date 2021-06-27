006af8596dd358be82afcd48170a15dacb2ff4ca
Oleg Prozorov <oleg@fraugster.com>
2021-06-27 11:50:15 +0200

    progressbar: create new item

--- progressbar/js/src/App.js
+++ progressbar/js/src/App.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {Row, Col, ProgressBar, Container, Badge} from 'react-bootstrap';
+import {Badge, Col, Container, ProgressBar, Row} from 'react-bootstrap';
 
 import './App.css';
 
@@ -31,9 +31,45 @@ const App = () => {
     const [items, setItems] = useState([])
     useEffect(loadItems, []);
 
+    const [title, setTitle] = useState([]);
+    const [percentComplete, setPercentComplete] = useState([]);
+
+    const onItemCreate = (event) => {
+        event.preventDefault()
+        console.log(title)
+        console.log(percentComplete)
+        let data = {
+            "dateStarted": "2021-01-02",
+            "tags": [],
+            "title": title,
+            "percentComplete": percentComplete
+        }
+        console.log(JSON.stringify(data))
+
+        fetch('http://localhost:3001/items', {
+            method: 'POST',
+            mode: 'cors',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(data)
+        })
+            .then(response => response.json())
+            .then(
+                res => loadItems(),
+                error => alert(error)
+            );
+    };
+
     return (
         <Container className="p-3">
             <ItemsList items={items}/>
+            <Col><Row>
+                <form onSubmit={onItemCreate}>
+                    <input type="text" onChange={ (e) => setTitle(e.target.value)} value={title} placeholder="Title"/>
+                    <input type="text" onChange={(e) => setPercentComplete(e.target.value)} value={percentComplete}
+                           placeholder="Percent Complete"/>
+                    <button type="submit">Add</button>
+                </form>
+            </Row></Col>
         </Container>
     );
 };
