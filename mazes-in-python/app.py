310a321879f916516406dcabfbe91247a3d99669
Oleg Prozorov <pro.oleg@gmail.com>
2021-09-12 00:51:10 +0200

    mazes-in-python: add seed param

--- mazes-in-python/app.py
+++ mazes-in-python/app.py
@@ -1,15 +1,17 @@
 import click
 
-from binary_tree import binary_tree
+from binary_tree import BinaryTree
 from grid import Grid
 
 
 @click.command()
 @click.option('--height', default=10, help='height of the maze')
 @click.option('--width', default=10, help='with of the maze')
-def main(height, width):
+@click.option('--direction', default="north", help='direction')
+@click.option('--seed', help='seed')
+def main(height, width, direction, seed):
     grid = Grid(height, width)
-    binary_tree(grid)
+    BinaryTree(seed).generate(direction, grid)
     click.echo(str(grid))
 
 
--- mazes-in-python/binary_tree.py
+++ mazes-in-python/binary_tree.py
@@ -1,11 +1,22 @@
-import random
+from __future__ import annotations
+from random import Random
+from operator import attrgetter
 
 from grid import Grid
 
 
 # todo test me
-def binary_tree(grid: Grid):
-    for cell in grid:
-        if ns := [c for c in [cell.north, cell.east] if c]:
-            if n := random.choice(ns):
-                cell.link(n)
+class BinaryTree:
+    dirs = ['north', 'east', 'south', 'west']
+    fs = [attrgetter(d) for d in dirs]
+
+    def __init__(self, seed: int | None = None) -> None:
+        self.rnd = Random(seed)
+
+    def generate(self, direction: str, grid: Grid):
+        index = self.dirs.index(direction)
+        for cell in grid:
+            cells = [self.fs[(index + i) % 4](cell) for i in [0, 1]]
+            if ns := [v for v in cells if v]:
+                if n := self.rnd.choice(ns):
+                    cell.link(n)
